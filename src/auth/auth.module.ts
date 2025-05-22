5bc4a8f420210bbe05b09f96ab55f9ab86a7ec4c
Artem Zhelikovskyi <zhelikovskiy.artem@gmail.com>
2025-05-22 14:05:20 +0200

    Move auth strategies to a separate folder

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,7 +4,7 @@ import { AuthController } from './auth.controller';
 import { PrismaModule } from 'src/prisma/prisma.module';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
-import { JwtStrategy } from './jwt.strategy';
+import { JwtStrategy } from './strategies/jwt.strategy';
 
 @Module({
     imports: [
